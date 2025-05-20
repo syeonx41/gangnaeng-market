8951359366b58c17e7f06be99e62359cf0a20ea1
SeungyeonWoo <seungyeon3287@gmail.com>
2025-05-20 15:09:08 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/com/gangnaeng/market/GangnaengMarketApplication.java
@@ -0,0 +1,13 @@
+package com.gangnaeng.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangnaengMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GangnaengMarketApplication.class, args);
+    }
+
+}
